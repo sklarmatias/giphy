4ea2c58a3c1e9ab7f19da174a3326d29550af0fe
Matias Sklar <msklar@frba.utn.edu.ar>
2024-04-25 10:55:04 -0300

    Implementación de API Rest en Laravel 10, con validación por OAuth, varios endpoints para obtener los datos que quiere el usuario y un middleware que loguea todos los requests recibidos.
    Se implementan los siguientes endpoints:
    
    1.- Identificar credenciales
    POST /api/v1/user/login
    
    2.- Buscar Gif por palabra
    GET /api/v1/user/gif/query
    
    3.- Buscar Gif por Gif_Id
    GET /api/v1/user/gif/get
    
    4.- Marcar Gif como Favorito
    POST /api/v1/user/favorite

--- giphy/routes/api/v1/api.php
+++ giphy/routes/api/v1/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/user')->group(function(){
     Route::post('/create', 'App\Http\Controllers\LoginController@create')->middleware('log.route');;
     Route::post('/login', 'App\Http\Controllers\LoginController@login')->middleware('log.route');;
-    //Esta ruta está protegida por el oauth
     Route::middleware('auth:api')->get('/gif/query','App\Http\Controllers\UserController@query')->middleware('log.route');;
     Route::middleware('auth:api')->get('/gif/get','App\Http\Controllers\UserController@getGifById')->middleware('log.route');;
     Route::middleware('auth:api')->post('/gif/favorite','App\Http\Controllers\UserController@saveAsFavorite')->middleware('log.route');;
